6477dc730cb2aaf746f18891916be7c62b25c913
GT CNIG DDU <79853274+GT-CNIG-DDU@users.noreply.github.com>
2022-09-02 16:22:59 +0200

    Delete setenv_AG.bat

--- schema/scripts/setenv_AG.bat
+++
@@ -1,3 +0,0 @@
-:: bureau
-set python=C:\Users\arnauld.gallais\AppData\Local\Programs\Python\Python310\python
-set frictionless=C:\Users\arnauld.gallais\AppData\Local\Programs\Python\Python310\python\Scripts\frictionless
