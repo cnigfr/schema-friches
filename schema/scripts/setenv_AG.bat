c675abdeda3c9dcfca078f433ae1ebbc28e7cb28
GT CNIG DDU <79853274+GT-CNIG-DDU@users.noreply.github.com>
2022-09-02 16:22:06 +0200

    Add files via upload

---
+++ schema/scripts/setenv_AG.bat
@@ -0,0 +1,3 @@
+:: bureau
+set python=C:\Users\arnauld.gallais\AppData\Local\Programs\Python\Python310\python
+set frictionless=C:\Users\arnauld.gallais\AppData\Local\Programs\Python\Python310\python\Scripts\frictionless
